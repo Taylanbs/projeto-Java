42f15bb4f19adb879bab71b0c8f820f36e55cc25
Taylanbs <taylanmf@gmail.com>
2023-11-28 00:26:37 -0300

    adicionando arquivo

--- DogSports/src/Sports/model/Modelo.java
+++ DogSports/src/Sports/model/Modelo.java
@@ -1,10 +1,21 @@
 package Sports.model;
 
-public class Modelo {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Modelo extends Produto {
+	private String modelo = "Modelo";
+	public Modelo(int numero, int tipo, String nome, float preco,String modelo) {
+		super(numero, tipo, nome, preco);
+		this.modelo = modelo;
 	}
-
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	//metodos espciais
+	@Override
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("O tipo do " + this.getTipo()+" é " + this.getModelo());
+	}
 }
--- DogSports/src/Sports/model/Produto.java
+++ DogSports/src/Sports/model/Produto.java
@@ -1,10 +1,73 @@
 package Sports.model;
 
-public class Produto {
+public abstract class Produto {
+	//atributos
+	private int numero,tipo;
+	private String nome;
+	private float preco;
+	
+	//Constructor
+	public Produto(int numero, int tipo, String nome, float preco) {
+		super();
+		this.numero = numero;
+		this.tipo = tipo;
+		this.nome = nome;
+		this.preco = preco;
+	}
+	//Metodos Getter e setter
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public int getNumero() {
+		return numero;
 	}
 
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+	//Metodos especiais
+	public void visualizar() {
+		String tipo = "";
+		switch(this.tipo) {
+		case 1:
+			tipo = "Modelo";
+			break;
+		case 2: 
+			tipo = "Tamanho";
+			break;
+		default:
+			
+		}
+		
+		System.out.println("Numero do produto: " + this.getNumero());
+		System.out.println("Nome do produto: "+this.getNome());
+		System.out.println("Tipo do produto: "+tipo);
+		System.out.println("Preço do produto: "+this.getPreco());
+		
+		
+	}
+	
+	
 }
